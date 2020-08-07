0153ae700759fb823184b3bf5ebd18b81080ae0a
8blank8 <69100249+8blank8@users.noreply.github.com>
2020-08-07 20:42:25 +0700

    nn

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+   $('.slider').slick({
+      arrows: true,
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 10000,
+      // centerMode: true,
+      centerPadding: '100px',
+      sladerToShow: 1,
+   });
+});
