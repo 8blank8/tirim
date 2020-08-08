409c16c5face44c102813bd946b9953ceb0d6d0e
8blank8 <69100249+8blank8@users.noreply.github.com>
2020-08-08 15:53:07 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,8 +4,22 @@ $(document).ready(function () {
       dots: true,
       autoplay: true,
       autoplaySpeed: 10000,
-      // centerMode: true,
       centerPadding: '100px',
       sladerToShow: 1,
+      responsive: [
+         {
+            breakpoint: 767,
+            settings: {
+               arrows: false,
+               autoplaySpeed: 5000,
+            }
+         }
+      ]
+   });
+});
+$(document).ready(function () {
+   $('.header__burger').click(function (event) {
+      $('.header__burger,.header__menu').toggleClass('active');
+      $('body').toggleClass('lock');
    });
 });
